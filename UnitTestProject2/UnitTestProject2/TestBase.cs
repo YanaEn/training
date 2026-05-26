07f43e04acd6b56dccb929f1a5a6458dc7a4341d
Yana <yanaengalycheva@yandex.ru>
2026-05-26 23:11:56 +0300

    Создан базовый класс TestBase. Перенесены в базовый класс все вспомогательные методы.

---
+++ UnitTestProject2/UnitTestProject2/TestBase.cs
@@ -0,0 +1,108 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressBookTests
+{
+    public class TestBase
+    {
+        protected IWebDriver driver;
+        private StringBuilder verificationErrors;
+        protected string baseURL;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "http://localhost/addressbook";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver?.Quit();
+            }
+            catch (Exception)
+            {
+
+            }
+            Assert.That(verificationErrors.ToString(), Is.Empty);
+        }
+        protected void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+        protected void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+        protected void GoToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+        protected void InitGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+        protected void FillGroupForm(GroupData group)
+        {
+            driver.FindElement(By.Name("group_name")).Click();
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+        }
+        protected void Logout()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        protected void ReturnToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("group page")).Click();
+        }
+
+        protected void SabmitGroupCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+        protected void GoToAddNewContact()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+        protected void SaveContact()
+        {
+            driver.FindElement(By.Name("theform")).Click();
+            driver.FindElement(By.XPath("//div[@id='content']/form/input[19]")).Click();
+        }
+
+        protected void Contact(ContactData contact)
+        {
+            driver.FindElement(By.Name("firstname")).Click();
+            driver.FindElement(By.Name("firstname")).Clear();
+            driver.FindElement(By.Name("firstname")).SendKeys(contact.Firstname);
+            driver.FindElement(By.Name("lastname")).Click();
+            driver.FindElement(By.Name("lastname")).Clear();
+            driver.FindElement(By.Name("lastname")).SendKeys(contact.Lastname);
+        }
+        protected void GoToHomePage()
+        {
+            driver.FindElement(By.LinkText("home page")).Click();
+        }
+    }
+}
